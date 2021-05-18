d4b9255b6747ef8a59dcfab923c6384eb335993c
C200026 <C200026@ccg.ac.jp>
2021-05-18 05:32:04 +0000

    New php docs2

---
+++ lec-02/self_increment.php
@@ -0,0 +1,13 @@
+<?php
+$a = 1;
+$b = ++$a;
+print "前置加算子<br />";
+print "a = {$a}<br />";
+print "b = {$b}<br />";
+
+$a = 1;
+$b = $a++;
+print "後置加算子<br />";
+print "a = {$a}<br />";
+print "b = {$b}<br />";
+?>
